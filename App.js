8c5d95b7e4a04707a1cfaf6ca37411c69e0213c7
Aimable <aimable.niyogakiza@drpgroup.com>
2019-08-06 06:58:50 +0100

    qrcode scan

---
+++ App.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import QRCode from 'react-native-qrcode'
+import { View, StyleSheet, TextInput } from 'react-native';
+
+
+export default class App extends React.Component {
+  state = {
+    text: 'http://facebook.github.io/react-native/',
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => this.setState({text: text})}
+          value={this.state.text}
+        />
+        <QRCode
+          value={this.state.text}
+          size={200}
+          bgColor='purple'
+          fgColor='white'/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        margin: 10,
+        borderRadius: 5,
+        padding: 5,
+    }
+});
